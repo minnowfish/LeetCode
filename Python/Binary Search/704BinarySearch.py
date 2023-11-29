3dd92ce7bfcdf362ab9870d4bb746c57610ecb14
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2023-11-29 21:02:28 +0800

    Update 704BinarySearch.py

--- Python/Binary Search/704BinarySearch.py
+++ Python/Binary Search/704BinarySearch.py
@@ -1,9 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums)-1
+        l, r = 0, len(nums)-1
         while l<=r:
-            mid = (l+ r)//2
+            mid = (l+ ((r-l)//2))
             if nums[mid] == target:
                 return(mid)
             elif nums[mid] < target:
@@ -11,5 +10,3 @@ class Solution:
             else:
                 r = mid - 1
         return(-1)
-
-#To be revisited (221 ms runtime, 17.96MB)
