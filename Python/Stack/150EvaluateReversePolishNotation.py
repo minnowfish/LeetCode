c0645db574108395a4b1b602fd5be829c7247803
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2024-07-17 15:55:24 +0800

    Create 150EvaluateReversePolishNotation.py

---
+++ Python/Stack/150EvaluateReversePolishNotation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in tokens:
+            if i not in ["+","-","*","/"]:
+                stack.append(int(i))
+            else:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                if i == "+":
+                    stack.append(num2 + num1)
+                elif i == "-":
+                    stack.append(num2-num1)
+                elif i == "/":
+                    stack.append(int(num2/num1))
+                else:
+                    stack.append(num2*num1)
+        
+        return(stack[0])
